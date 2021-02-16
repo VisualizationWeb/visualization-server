61aa6930c603aa26b20c0491c251117e905be27d
solo5star <solo_5@naver.com>
2021-02-16 22:39:05 +0900

    vschat/models.py: Use DateField instead of CharField

--- vschat/models.py
+++ vschat/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class StepCount_Data(models.Model):
-    saved_time = models.CharField(primary_key = True, max_length = 30)
+    saved_time = models.DateField(primary_key=True)
     stepCount = models.IntegerField()
 
     def __str__(self):
